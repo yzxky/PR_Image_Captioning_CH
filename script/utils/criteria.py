c02ce1ca9ff08b53ccbdeef1d98b6284823bbeef
yzxky <xky16@mails.tsinghua.edu.cn>
2017-06-07 15:51:01 +0800

    zrb0607:BLEU score

---
+++ script/utils/criteria.py
@@ -0,0 +1,13 @@
+import nltk
+
+# calculate the ngram of BLEU score
+####### input ########
+# reference: reference sentence lists Array (eg:[[1,2,3],[4,5,6]])
+# predict:   predict sentence list
+# n:         ngram
+##### output ########
+# BLEUscore: BLEU-n score
+
+def bleu_score(reference,predict,n):
+    BLEUscore = nltk.translate.bleu_score.modified_precision(reference,predict,n)
+    return float(BLEUscore)
